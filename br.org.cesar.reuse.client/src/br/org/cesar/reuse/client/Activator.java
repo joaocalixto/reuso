d949dffcd6f19dae1bcc210d5f9c49e5a78e1035
Aécio Costa <aeciovc@gmail.com>
2014-08-05 12:06:55 -0300

    Projet Client

---
+++ br.org.cesar.reuse.client/src/br/org/cesar/reuse/client/Activator.java
@@ -0,0 +1,40 @@
+package br.org.cesar.reuse.client;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.http.HttpService;
+
+public class Activator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		Activator.context = bundleContext;
+		
+		ServiceReference<HttpService> sr = context.getServiceReference(HttpService.class);
+		HttpService http = (HttpService)context.getService(sr);
+		
+		if (http != null) {
+			http.registerServlet("/ui", new MyServlet(), null, null);
+			System.out.println("Servlet Registrado");
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.context = null;
+	}
+
+}
---
+++ br.org.cesar.reuse.client/src/br/org/cesar/reuse/client/MyServlet.java
@@ -0,0 +1,21 @@
+package br.org.cesar.reuse.client;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("<html><body>oi</body></html>");
+	}
+
+}
