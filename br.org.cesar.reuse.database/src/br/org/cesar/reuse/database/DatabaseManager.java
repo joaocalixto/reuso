b693e5f67e8f8fd8bb7115ca5c5887e25254da4b
Igor Morais <igor.morais@cesar.org.br>
2014-08-10 16:47:58 -0300

    Bug fix

--- br.org.cesar.reuse.database/src/br/org/cesar/reuse/database/DatabaseManager.java
+++ br.org.cesar.reuse.database/src/br/org/cesar/reuse/database/DatabaseManager.java
@@ -11,42 +11,54 @@ import br.org.cesar.reuse.commons.model.User;
 public class DatabaseManager {
 
 	private final Map<Long, User> userMap = new HashMap<>();
-	
+
 	private final Map<Long, Repair> repairMap = new HashMap<>();
-	
+
 	static class LazyLoader {
-		static final DatabaseManager INSTANCE = new DatabaseManager();                                               
+		static final DatabaseManager INSTANCE = new DatabaseManager();
 	}
-	
+
 	public static DatabaseManager getInstance() {
 		return LazyLoader.INSTANCE;
 	}
-	
-	public void addUser(User user) {
+
+	public void addUser(final User user) {
 		userMap.put(user.getId(), user);
 	}
-	
-	public void removeUser(long id) {
-		userMap.remove(id);
+
+	public void removeUser(final long id) {
+
+		for (final Map.Entry<Long, User> entry : userMap.entrySet()) {
+
+			if (entry.getValue().getId() == id) {
+				userMap.remove(entry.getKey());
+			}
+		}
 	}
-	
-	public void removeUser(User user) {
+
+	public void removeUser(final User user) {
 		userMap.remove(user);
 	}
-	
-	public void addRepair(Repair repair) {
+
+	public void addRepair(final Repair repair) {
 		repairMap.put(repair.getId(), repair);
 	}
-	
+
 	public List<Repair> getAllRepair() {
-		return new  ArrayList<Repair>(repairMap.values());
+		return new ArrayList<Repair>(repairMap.values());
 	}
-	
-	public void removeRepair(long id) {
-		repairMap.remove(id);
+
+	public void removeRepair(final long id) {
+
+		for (final Map.Entry<Long, Repair> entry : repairMap.entrySet()) {
+
+			if (entry.getValue().getId() == id) {
+				repairMap.remove(entry.getKey());
+			}
+		}
 	}
-	
-	public void removeRepair(Repair repair) {
+
+	public void removeRepair(final Repair repair) {
 		repairMap.remove(repair);
 	}
 }
