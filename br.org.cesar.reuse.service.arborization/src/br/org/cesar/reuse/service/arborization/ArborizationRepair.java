44c88a14a2ecccf90d71b1bfe052b80c47ec3760
Igor Morais <igor.morais@cesar.org.br>
2014-08-12 13:56:44 -0300

    Minor update

---
+++ br.org.cesar.reuse.service.arborization/src/br/org/cesar/reuse/service/arborization/ArborizationRepair.java
@@ -0,0 +1,7 @@
+package br.org.cesar.reuse.service.arborization;
+
+import br.org.cesar.reuse.commons.model.Repair;
+
+public class ArborizationRepair extends Repair {
+
+}
---
+++ br.org.cesar.reuse.service.lighting/src/br/org/cesar/reuse/service/lighting/LightingRepeair.java
@@ -0,0 +1,7 @@
+package br.org.cesar.reuse.service.lighting;
+
+import br.org.cesar.reuse.commons.model.Repair;
+
+public class LightingRepeair extends Repair {
+
+}
