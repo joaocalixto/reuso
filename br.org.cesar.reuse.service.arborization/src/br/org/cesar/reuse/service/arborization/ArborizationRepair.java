3e7c47b982acb141b49c5b89fca43d0e033ab44d
Igor Morais <igor.morais@cesar.org.br>
2014-08-12 14:35:17 -0300

    Update atributes

--- br.org.cesar.reuse.service.arborization/src/br/org/cesar/reuse/service/arborization/ArborizationRepair.java
+++ br.org.cesar.reuse.service.arborization/src/br/org/cesar/reuse/service/arborization/ArborizationRepair.java
@@ -1,7 +1,33 @@
 package br.org.cesar.reuse.service.arborization;
 
+import java.util.List;
+
 import br.org.cesar.reuse.commons.model.Repair;
 
 public class ArborizationRepair extends Repair {
 
+	private String treeType;
+
+	/**
+	 * @return the treeType
+	 */
+	public final String getTreeType() {
+		return treeType;
+	}
+
+	/**
+	 * @param treeType
+	 *            the treeType to set
+	 */
+	public final void setTreeType(final String treeType) {
+		this.treeType = treeType;
+	}
+
+	public static List<String> getAtributes() {
+		final List<String> atributesList = Repair.getAtributes();
+
+		atributesList.add("Tipo da Árvore");
+
+		return atributesList;
+	}
 }
--- br.org.cesar.reuse.service.lighting/src/br/org/cesar/reuse/service/lighting/LightingRepeair.java
+++ br.org.cesar.reuse.service.lighting/src/br/org/cesar/reuse/service/lighting/LightingRepeair.java
@@ -1,7 +1,33 @@
 package br.org.cesar.reuse.service.lighting;
 
+import java.util.List;
+
 import br.org.cesar.reuse.commons.model.Repair;
 
 public class LightingRepeair extends Repair {
 
+	private String lightingType;
+
+	/**
+	 * @return the lightingType
+	 */
+	public final String getLightingType() {
+		return lightingType;
+	}
+
+	/**
+	 * @param lightingType
+	 *            the lightingType to set
+	 */
+	public final void setLightingType(final String lightingType) {
+		this.lightingType = lightingType;
+	}
+
+	public static List<String> getAtributes() {
+		final List<String> atributesList = Repair.getAtributes();
+
+		atributesList.add("Tipo da Iluminação");
+
+		return atributesList;
+	}
 }
